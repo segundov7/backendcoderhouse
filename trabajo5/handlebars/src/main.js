3205c46919944091b1188eedf47f0ba884a5ef54
segundov7 <seguvallejos3050@gmail.com>
2022-08-16 21:25:37 -0300

    trabajo 5

---
+++ trabajo5/handlebars/src/main.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const ProductosApi = require('../api/productos.js');
+const productosApi = new ProductosApi()
+
+let conProductos = false
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
+
+app.set('views', './views')
+app.set('view engine', 'hbs');
+
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        defaultLayout: 'index',
+        layoutsDir: './views/layouts',
+        partialDir: __dirname + '/views/partials'
+    })
+);
+
+
+app.post('/productos', (req, res) => {
+    productosApi.save(req.body);
+    res.redirect('/');
+})
+
+app.get('/productos', (req, res) => {
+    const productos = productosApi.listAll()
+    conProductos = productos.length > 0 ? true : false;
+    res.render('vista', {productos, conProductos});
+});
+
+
+const PORT = 8080
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+server.on("error", error => console.log(`Error en servidor ${error}`));
---
+++ trabajo5/pug/src/main.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const ProductosApi = require('../api/productos.js');
+const productosApi = new ProductosApi()
+
+let conProductos = false
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
+app.set('views', './views')
+app.set('view engine', 'pug');
+
+
+ app.post('/productos', (req, res) => {
+     productosApi.save(req.body);
+     res.redirect('/');
+ })
+
+ app.get('/productos', (req, res) => {
+     const productos = productosApi.listAll()
+     conProductos = productos.length > 0 ? true : false;
+     res.render('vista', {productos, conProductos});
+ });
+
+const PORT = 8080
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+server.on("error", error => console.log(`Error en servidor ${error}`));
